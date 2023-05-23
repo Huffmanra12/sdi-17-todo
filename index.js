318277209e491af7be5e124fc4d1d97d142b7442
Richard <huffmanra12@gmail.com>
2023-05-23 14:07:41 -0600

    Created javascript code to add list items and remove list items, Also styled document

---
+++ index.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function(){
+//Stores an array of items to list
+//variable storing items to be manipulated
+var itemStore = [];
+var listItem = document.querySelector('#item-input');
+var listElement = document.querySelector('#list');
+var NewItem = document.querySelector('#Add-Item');
+var RemoveItem = document.querySelector('#Remove-Item');
+
+NewItem.addEventListener(
+  "click", function(){itemStore.push(listItem.value)
+  }
+);
+NewItem.addEventListener(
+  'click', function(){
+      let newParagraph = document.createElement('p');
+      newParagraph.textContent = listItem.value;
+      newParagraph.id = listItem.value;
+      listElement.appendChild(newParagraph);
+    console.log(itemStore);
+  }
+);
+
+RemoveItem.addEventListener(
+  'click', function(){
+    let Item = document.getElementById(listItem.value);
+    let ItemIndex = itemStore.indexOf(Item);
+    itemStore.splice(ItemIndex, 1);
+    Item.remove();
+    console.log(itemStore);
+  })
+});
